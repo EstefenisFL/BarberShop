235996658859758d77335451c4eb736015aee61f
Estêfenis Freitas <lopefl89@gmail.com>
2022-05-18 09:01:55 -0300

    add router and visual layouts to home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AgendamentoComponent } from './agendamento/agendamento.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ServicosComponent } from './servicos/servicos.component';
+import { SobreComponent } from './sobre/sobre.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home',
+    component: HomeComponent
+  },
+  {
+    path:'cadastro',
+    component: CadastroComponent
+  },
+  {
+    path:'agendamento',
+    component: AgendamentoComponent
+  },
+  {
+    path:'serviços',
+    component: ServicosComponent
+  },
+  {
+    path:'sobre',
+    component: SobreComponent
+  },
+  {
+    path:'login',
+    component: LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { LoginComponent } from './login/login.component';
+import { AgendamentoComponent } from './agendamento/agendamento.component';
+import { ServicosComponent } from './servicos/servicos.component';
+import { SharedComponent } from './shared/shared.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SobreComponent,
+    CadastroComponent,
+    LoginComponent,
+    AgendamentoComponent,
+    ServicosComponent,
+    SharedComponent
   ],
   imports: [
     BrowserModule,
